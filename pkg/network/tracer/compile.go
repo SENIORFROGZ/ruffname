36ee251856c22b1047f6eb944867febe5d59789a
Sex.com <cajungotshop@gmail.com>
2021-09-20 14:59:29 -0500

    Initialize bryce.kahle/conntrack-cgo

---
+++ pkg/network/tracer/compile.go
@@ -0,0 +1,26 @@
+// +build linux_bpf
+
+package tracer
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/ebpf/bytecode/runtime"
+	"github.com/DataDog/datadog-agent/pkg/network/config"
+)
+
+//go:generate go run ../../ebpf/include_headers.go ../ebpf/c/runtime/conntrack.c ../../ebpf/bytecode/build/runtime/conntrack.c ../ebpf/c ../ebpf/c/runtime ../../ebpf/c
+//go:generate go run ../../ebpf/bytecode/runtime/integrity.go ../../ebpf/bytecode/build/runtime/conntrack.c ../../ebpf/bytecode/runtime/conntrack.go runtime
+
+func getRuntimeCompiledConntracker(config *config.Config) (runtime.CompiledOutput, error) {
+	return runtime.Conntrack.Compile(&config.Config, getCFlags(config))
+}
+
+func getCFlags(config *config.Config) []string {
+	var cflags []string
+	if config.CollectIPv6Conns {
+		cflags = append(cflags, "-DFEATURE_IPV6_ENABLED")
+	}
+	if config.BPFDebug {
+		cflags = append(cflags, "-DDEBUG=1")
+	}
+	return cflags
+}
