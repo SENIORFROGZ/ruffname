855db93c8c1ec307c14b39a2ac95afde3fa5c2cd
Sex.com <cajungotshop@gmail.com>
2021-09-20 13:54:06 -0500

    Initialize pedro/http-userspace-improvements

---
+++ pkg/network/http/http_statkeeper.go
@@ -0,0 +1,48 @@
+// +build linux_bpf
+
+package http
+
+import (
+	"C"
+)
+
+type httpStatKeeper struct {
+	stats  map[Key]map[string]RequestStats
+	buffer []byte
+}
+
+func newHTTPStatkeeper() *httpStatKeeper {
+	return &httpStatKeeper{
+		stats:  make(map[Key]map[string]RequestStats),
+		buffer: make([]byte, HTTPBufferSize),
+	}
+}
+
+func (h *httpStatKeeper) Process(transactions []httpTX) {
+	for _, tx := range transactions {
+		key := Key{
+			SourceIP:   tx.SourceIP(),
+			DestIP:     tx.DestIP(),
+			SourcePort: tx.SourcePort(),
+			DestPort:   tx.DestPort(),
+		}
+		path := tx.Path(h.buffer)
+		statusClass := tx.StatusClass()
+		latency := tx.RequestLatency()
+
+		if _, ok := h.stats[key]; !ok {
+			h.stats[key] = make(map[string]RequestStats)
+		}
+		stats := h.stats[key][string(path)]
+		stats.AddRequest(statusClass, latency)
+		h.stats[key][string(path)] = stats
+	}
+}
+
+func (h *httpStatKeeper) GetAndResetAllStats() map[Key]map[string]RequestStats {
+	ret := h.stats // No deep copy needed since `h.stats` gets reset
+	h.stats = make(map[Key]map[string]RequestStats)
+	return ret
+}
+
+func (h *httpStatKeeper) Close() {}
