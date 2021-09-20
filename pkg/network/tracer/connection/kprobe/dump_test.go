fd7fbedc1d70c1b099944b78d8941fcfb4a7303d
Sex.com <cajungotshop@gmail.com>
2021-09-20 15:06:48 -0500

    Initialize nplanel/dump-ebpf-info

---
+++ pkg/network/tracer/connection/kprobe/dump_test.go
@@ -0,0 +1,21 @@
+//go:build linux_bpf
+// +build linux_bpf
+
+package kprobe
+
+import (
+	"testing"
+	//	"github.com/DataDog/ebpf"
+	netebpf "github.com/DataDog/datadog-agent/pkg/network/ebpf"
+)
+
+func TestManagerDumpHandler(t *testing.T) {
+	//	closedHandler *ebpf.PerfHandler, runtimeTracer bool) *manager.Manager {
+	m := netebpf.NewManager(nil, false)
+	setupDumpHandler(m)
+
+	t.Log("==================ok")
+	for _, emap := range m.Maps {
+		t.Log(emap.DumpHandler)
+	}
+}
