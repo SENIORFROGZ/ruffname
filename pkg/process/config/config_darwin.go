b25174d0de6faf32ed468793f787b92ff4dad0d2
Sex.com <cajungotshop@gmail.com>
2021-09-20 14:27:13 -0500

    Initialize mike.zhu/probewin1

---
+++ pkg/process/config/config_darwin.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+const (
+	defaultLogFilePath = "/opt/datadog-agent/logs/process-agent.log"
+
+	// defaultSystemProbeLogFilePath is the default logging file for the system probe
+	defaultSystemProbeLogFilePath = "/opt/datadog-agent/logs/system-probe.log"
+
+	// Agent 6
+	defaultDDAgentBin = "/opt/datadog-agent/bin/agent/agent"
+
+	// defaultSystemProbeAddress is the default unix socket path to be used for connecting to the system probe
+	defaultSystemProbeAddress = "/opt/datadog-agent/run/sysprobe.sock"
+)
+
+// ValidateSysprobeSocket validates that the sysprobe socket config option is of the correct format.
+func ValidateSysprobeSocket(sockPath string) error {
+	if !filepath.IsAbs(sockPath) {
+		return fmt.Errorf("socket path must be an absolute file path")
+	}
+	return nil
+}
